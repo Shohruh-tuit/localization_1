0bd2d393b315f6242a4fbc65ca83b2616f985d28
Shohruh Masharipov <shohruhmasharipov49@gmail.com>
2021-04-27 13:35:19 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:pdp_s2_localization/pages/home_page.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+        supportedLocales: [
+          Locale('en', 'US'),
+          Locale('ru', 'RU'),
+          Locale('uz', 'UZ'),
+          Locale('fr', 'FR'),
+
+          // http://www.lingoes.net/en/translator/langcode.htm
+        ],
+        path:
+            'assets/translations', // <-- change the path of the translation files
+        fallbackLocale: Locale('en', 'US'),
+        child: MyApp()),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("localization").tr(),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(18),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(
+              child: Center(
+                child: Text(
+                  "welcome",
+                  style: TextStyle(fontSize: 20),
+                ).tr(),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: FlatButton(
+                    height: 45,
+                    color: Colors.green,
+                    onPressed: () {
+                      context.locale = Locale('en', 'US');
+                    },
+                    child: Text(
+                      "English",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8),
+                Expanded(
+                  child: FlatButton(
+                    height: 45,
+                    color: Colors.red,
+                    onPressed: () {
+                      context.locale = Locale('ru', 'RU');
+                    },
+                    child: Text(
+                      "Russian",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8),
+                Expanded(
+                  child: FlatButton(
+                    height: 45,
+                    color: Colors.blue,
+                    onPressed: () {
+                      context.locale = Locale('uz', 'UZ');
+                    },
+                    child: Text(
+                      "Uzbek",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8),
+                Expanded(
+                  child: FlatButton(
+                    height: 45,
+                    color: Colors.yellow[900],
+                    onPressed: () {
+                      context.locale = Locale('fr', 'FR');
+                    },
+                    child: Text(
+                      "French",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
